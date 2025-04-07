b68436bddcd441deb8d07340ca6189a8e63ad638
Júlio Gabriel Caetano Dos Santos <s1zadev07@gmail.com>
2025-04-07 16:26:41 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Menu Hambúrguer
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menuIcon');
+    const navLinks = document.getElementById('navLinks');
+
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
